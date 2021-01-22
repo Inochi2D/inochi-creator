b134dae8cdc0e11a6d8c39c03a4983cad32a4642
Member1221 <cli-luna@protonmail.com>
2021-01-22 02:10:08 +0100

    Fix comment string

--- source/core/utils.d
+++ source/core/utils.d
@@ -1,9 +1,9 @@
 module core.utils;
 import gtk.CssProvider;
 
-/++
+/**
     Returns a CSS provider from a string of css data.
-+/
+*/
 CssProvider styleFromString(string styleSheet) {
     CssProvider provider = new CssProvider();
     provider.loadFromData(styleSheet);
