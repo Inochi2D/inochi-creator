cb040c0e809ab46f2207a5de52238d64863e6836
LunaTheFoxgirl <cli-luna@protonmail.com>
2022-03-17 02:39:34 +0100

    Dummy tracking boilerplate

--- source/creator/panels/tracking.d
+++ source/creator/panels/tracking.d
@@ -21,14 +21,22 @@ class TrackingPanel : Panel {
 private:
     bool trackVMC;
     bool trackOSF;
+    bool trackDummy;
+
+    // vmc
     int portN = 39540;
 
+
+    // osf
+
+    // dummy
 protected:
     override
     void onUpdate() {
         // TODO: check if in model test mode
 
         if (igCheckbox(__("VMC Receiver"), &trackVMC)) trackOSF = false;
+        incTooltip(_("A reciever which uses your phone and associated app to track your body"));
         if (trackVMC) {
             if (igInputInt(__("Port"), &portN, 1, 1000, ImGuiInputTextFlags.None)) {
                 portN = clamp(portN, 0, 65535);
@@ -36,6 +44,13 @@ protected:
         }
 
         if (igCheckbox(__("OpenSeeFace Receiver"), &trackOSF)) trackVMC = false;
+        incTooltip(_("A reciever which uses a webcam connected to your computer to track your body"));
+        if (trackOSF) {
+
+        }
+
+        if (igCheckbox(__("Dummy Receiver"), &trackDummy)) trackVMC = false;
+        incTooltip(_("A reciever which randomly adjusts chosen parameters"));
         if (trackOSF) {
 
         }
