acd9f5cfc9cad171da06ba3d9c87914a204223df
Joshua T. Fisher <playmer@gmail.com>
2022-05-30 23:41:11 -0700

    Correction of version

--- source/creator/core/package.d
+++ source/creator/core/package.d
@@ -119,7 +119,7 @@ void incOpenWindow() {
     enforce(sdlSupport != SDLSupport.badLibrary, "Bad SDL2 library found!");
     
     // Luna please tell me how to make this elegant in the PR.
-    version(BindImGui_Static_DynamicCRT)
+    version(BindImGui_Static)
     {
     }
     else 
