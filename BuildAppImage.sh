e609a52936419da0878bf6a8f7fa4ee7f1d08053
Joshua T. Fisher <playmer@gmail.com>
2022-06-08 22:54:37 -0700

    AppImage fix

--- BuildAppImage.sh
+++ BuildAppImage.sh
@@ -8,6 +8,12 @@ mkdir usr/bin
 cp ../../out/inochi-creator usr/bin/inochi-creator
 cp ../../res/logo_256.png logo_256.png
 cp ../../res/inochi-creator.desktop inochi-creator.desktop
+cp ../../res/AppRun AppRun
+cp ../../res/NotoSansCJK-Regular-LICENSE usr/bin/NotoSansCJK-Regular-LICENSE
+cp ../../res/MaterialIcons-LICENSE usr/bin/MaterialIcons-LICENSE
+cp ../../res/OpenDyslexic-LICENSE usr/bin/AppRun
+cp ../../LICENSE usr/bin/LICENSE
+
 cd ..
 
-appimagetool.AppImage inochi-creator.AppDir
+ARCH=x86_64 appimagetool.AppImage inochi-creator.AppDir
