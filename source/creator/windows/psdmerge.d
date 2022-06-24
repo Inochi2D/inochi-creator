4a9f9497c7ece20c6aa27acb8f39890daf2f0018
LunaTheFoxgirl <cli-luna@protonmail.com>
2022-06-25 01:56:12 +0200

    Basic PSD merging

--- source/creator/windows/psdmerge.d
+++ source/creator/windows/psdmerge.d
@@ -7,21 +7,109 @@
 module creator.windows.psdmerge;
 import creator.windows;
 import creator.core;
+import creator;
+import creator.ext;
 import std.string;
 import creator.utils.link;
 import inochi2d;
 import i18n;
 import psd;
 
+/**
+    Binding between layer and node
+*/
+struct NodeLayerBinding {
+    Layer layer;
+    Texture layerTexture;
+
+    Node node;
+    bool replaceTexture;
+    string layerPath;
+}
+
 class PSDMergeWindow : Window {
 private:
     PSD document;
+    NodeLayerBinding[] bindings;
+
+    void populateBindings() {
+        import std.array : join;
+        auto puppet = incActivePuppet();
+        auto parts = puppet.findNodesType!ExPart(puppet.root);
+
+        ExPart findPartForSegment(string segment) {
+            foreach(ref ExPart part; parts) {
+                if (part.layerPath == segment) return part;
+            }
+            return null;
+        }
+
+        string[] layerPathSegments;
+        foreach_reverse(layer; document.layers) {
+
+            // Build layer path segments
+            if (layer.type != LayerType.Any) {
+                if (layer.name != "</Layer set>") layerPathSegments ~= layer.name; 
+                else layerPathSegments.length--;
+                continue;
+            }
+
+            // Load texture in to memory
+            layer.extractLayerImage();
+            inTexPremultiply(layer.data);
+            auto layerTexture = new Texture(layer.data, layer.width, layer.height);
+
+            // See if any matching segments can be found
+            string currSegment = "/"~layerPathSegments.join("/");
+            ExPart seg = findPartForSegment(currSegment);
+            if (seg) {
+
+                // If so, default to replace
+                bindings ~= NodeLayerBinding(layer, layerTexture, seg, true);
+            } else {
+
+                // Otherwise, default to add
+                bindings ~= NodeLayerBinding(layer, layerTexture, puppet.root, false);
+            }
+        }
+    }
+
+    void apply() {
+        auto puppet = incActivePuppet();
+
+        // Apply all the bindings to the node tree.
+        foreach(binding; bindings) {
+            if (binding.replaceTexture) {
+                (cast(ExPart)binding.node).textures[0] = binding.layerTexture;
+            } else {
+                incCreateExPart(binding.layerTexture, binding.node, binding.layer.name);
+            }
+        }
+        
+        // Unload PSD, we're done with it
+        destroy(document);
+
+        // Repopulate texture slots, removing unused textures
+        puppet.populateTextureSlots();
+    }
+
+    void layerView() {
+
+    }
+
+    void treeView() {
+
+    }
 
 protected:
 
     override
     void onUpdate() {
-
+        if (igButton(__("Merge"))) {
+            apply();
+            this.close();
+            return;
+        }
     }
 
 public:
@@ -31,6 +119,7 @@ public:
 
     this(string path) {
         document = parseDocument(path);
+        this.populateBindings();
         super(_("PSD Merging"));
     }
 }
