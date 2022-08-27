413b17a7b430463e08e6cfa18e217d6c4cbd65da
LunaTheFoxgirl <cli-luna@protonmail.com>
2022-08-27 05:36:18 +0200

    Texture slot: handle edge cases

--- source/creator/widgets/texture.d
+++ source/creator/widgets/texture.d
@@ -5,6 +5,7 @@ import bindbc.imgui;
 import inmath;
 import inochi2d;
 import std.math : quantize;
+import bindbc.opengl : GL_RGBA;
 
 /**
     Renders a texture slot with specified size
@@ -12,40 +13,48 @@ import std.math : quantize;
 void incTextureSlot(string text, Texture texture, ImVec2 size = ImVec2(92, 92), float gridSize = 32) {
     if (igBeginChildFrame(igGetID(text.ptr, text.ptr+text.length), size, ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)) {
 
+        ImVec2 startPos;
+        igGetCursorPos(&startPos);
+        
+        float paddingX = igGetStyle().FramePadding.x;
+        float paddingY = igGetStyle().FramePadding.y;
+
+
         igBeginGroup();
-            ImVec2 startPos;
-            igGetCursorPos(&startPos);
-            float paddingX = igGetStyle().FramePadding.x;
-            float paddingY = igGetStyle().FramePadding.y;
 
-            float qsizex = quantize(size.x, gridSize/2);
-            float qsizey = quantize(size.y, gridSize/2);
-
-            ImVec2 screenStart;
-            auto drawList = igGetWindowDrawList();
-            igGetCursorScreenPos(&screenStart);
-
-            // Draw background grid
-            ImVec2 gridMin = ImVec2(screenStart.x-(paddingX/2), screenStart.y-(paddingY/2));
-            ImVec2 gridMax = ImVec2(gridMin.x+size.x-paddingX, gridMin.y+size.y-paddingY);
-            ImDrawList_AddImageRounded(
-                drawList,
-                cast(ImTextureID)incGetGrid().getTextureId(),
-                gridMin,
-                gridMax,
-                ImVec2(0, 0),
-                ImVec2(
-                    clamp(quantize((qsizex/gridSize), 0.5), 1, float.max),
-                    clamp(quantize((qsizey/gridSize), 0.5), 1, float.max),
-                ),
-                0xFFFFFFFF,
-                igGetStyle().FrameRounding
-            );
-
-            igSetCursorPos(startPos);
-
+            // Only draw texture stuff if we actually have a texture
             if (texture) {
-            
+
+                // NOTE: These variables may be reused if the texture is square.
+                float qsizex = quantize(size.x, gridSize/2);
+                float qsizey = quantize(size.y, gridSize/2);
+
+                ImVec2 screenStart;
+                auto drawList = igGetWindowDrawList();
+                igGetCursorScreenPos(&screenStart);
+
+                // Draw background grid
+                ImVec2 gridMin = ImVec2(screenStart.x-(paddingX/2), screenStart.y-(paddingY/2));
+                ImVec2 gridMax = ImVec2(gridMin.x+size.x-paddingX, gridMin.y+size.y-paddingY);
+
+                // Draw transparent background if there is any transparency
+                if (texture.colorMode == GL_RGBA) {
+                    ImDrawList_AddImageRounded(
+                        drawList,
+                        cast(ImTextureID)incGetGrid().getTextureId(),
+                        gridMin,
+                        gridMax,
+                        ImVec2(0, 0),
+                        ImVec2(
+                            clamp(quantize((qsizex/gridSize), 0.5), 1, float.max),
+                            clamp(quantize((qsizey/gridSize), 0.5), 1, float.max),
+                        ),
+                        0xFFFFFFFF,
+                        igGetStyle().FrameRounding
+                    );
+                }
+
+                // Calculate padded size
                 float paddedSizeX = size.x-(paddingX*2);
                 float paddedSizeY = size.y-(paddingY*2);
 
@@ -53,17 +62,40 @@ void incTextureSlot(string text, Texture texture, ImVec2 size = ImVec2(92, 92), 
                 float widthScale = paddedSizeX / cast(float)texture.width;
                 float heightScale = paddedSizeY / cast(float)texture.height;
                 float scale = min(widthScale, heightScale);
-                
-                vec4 bounds = vec4(0, 0, texture.width*scale, texture.height*scale);
-                if (widthScale > heightScale) bounds.x = (paddedSizeX-bounds.z)/2;
-                else if (widthScale < heightScale) bounds.y = (paddedSizeY-bounds.w)/2;
 
-                // Draw texture preview
-                igSetCursorPos(ImVec2(startPos.x+bounds.x, startPos.y+bounds.y));
-                igImage(
-                    cast(ImTextureID)texture.getTextureId(),
-                    ImVec2(bounds.z, bounds.w)
-                );
+                if (widthScale == heightScale) {
+
+                    // Texture is square
+                    ImDrawList_AddImageRounded(
+                        drawList,
+                        cast(ImTextureID)texture.getTextureId(),
+                        gridMin,
+                        gridMax,
+                        ImVec2(0, 0),
+                        ImVec2(
+                            clamp(quantize((qsizex/gridSize), 0.5), 1, float.max),
+                            clamp(quantize((qsizey/gridSize), 0.5), 1, float.max),
+                        ),
+                        0xFFFFFFFF,
+                        igGetStyle().FrameRounding
+                    );
+
+                } else {
+
+                    // Texture is non-square
+                    igSetCursorPos(startPos);
+                
+                    vec4 bounds = vec4(0, 0, texture.width*scale, texture.height*scale);
+                    if (widthScale > heightScale) bounds.x = (paddedSizeX-bounds.z)/2;
+                    else if (widthScale < heightScale) bounds.y = (paddedSizeY-bounds.w)/2;
+
+                    // Draw texture preview
+                    igSetCursorPos(ImVec2(startPos.x+bounds.x, startPos.y+bounds.y));
+                    igImage(
+                        cast(ImTextureID)texture.getTextureId(),
+                        ImVec2(bounds.z, bounds.w)
+                    );
+                }
             }
         igEndGroup();
         
