5d2c291aa421dabc570c3aeaf620db54d0bbcc61
LunaTheFoxgirl <cli-luna@protonmail.com>
2022-10-01 19:57:07 +0200

    incTextBordered

--- source/creator/widgets/label.d
+++ source/creator/widgets/label.d
@@ -46,6 +46,29 @@ void incTextShadowed(string text) {
 }
 
 /**
+    Renders text with a slight border
+*/
+void incTextBordered(string text) {
+    
+    ImVec2 origin;
+    igGetCursorPos(&origin);
+    
+    // Shadow
+    igSetCursorPos(ImVec2(origin.x+1, origin.y));
+    incTextColored(ImVec4(0.25, 0.25, 0.25, 0.8), text);
+    igSetCursorPos(ImVec2(origin.x-1, origin.y));
+    incTextColored(ImVec4(0.25, 0.25, 0.25, 0.8), text);
+    igSetCursorPos(ImVec2(origin.x, origin.y+1));
+    incTextColored(ImVec4(0.25, 0.25, 0.25, 0.8), text);
+    igSetCursorPos(ImVec2(origin.x, origin.y-1));
+    incTextColored(ImVec4(0.25, 0.25, 0.25, 0.8), text);
+
+    // Version String
+    igSetCursorPos(origin);
+    incText(text);
+}
+
+/**
     Render wrapped
 */
 void incTextWrapped(string text) {
