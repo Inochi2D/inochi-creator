d5eebbb7274f813bbce1dddc4ca41436418ec5ea
seagetch <sigetch@gmail.com>
2022-10-16 14:24:57 +0900

    Added implementation of automesh (WIP)

---
+++ source/creator/viewport/common/automesh/automesh.d
@@ -0,0 +1,10 @@
+module creator.viewport.common.automesh.automesh;
+
+import creator.viewport.common.mesh;
+import inochi2d.core;
+
+class AutoMeshProcessor {
+public:
+    abstract IncMesh autoMesh(Drawable targets, IncMesh meshData);
+
+};
---
+++ source/creator/viewport/common/automesh/package.d
@@ -0,0 +1,4 @@
+module creator.viewport.common.automesh;
+
+public import creator.viewport.common.automesh.automesh;
+public import creator.viewport.common.automesh.contours;
