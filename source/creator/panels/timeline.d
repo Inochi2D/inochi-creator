2de50317194606753c21f984249fffa0d0ea7f24
Luna <cli-luna@protonmail.com>
2023-02-03 02:25:40 +0900

    Bottom toolbar for timeline

--- source/creator/panels/timeline.d
+++ source/creator/panels/timeline.d
@@ -129,108 +129,127 @@ protected:
     void onUpdate() {
         bool inAnimMode = incEditMode() == EditMode.AnimEdit;
 
-        igBeginDisabled(!inAnimMode);
-        if (incBeginInnerToolbar(24)) {
-            
-            if (incToolbarButton("", 32)) {
-                incActivePuppet().player.stopAll(true);
-                playing = false;
-            }
-
-            if (incToolbarButton(playing ? "" : "", 32)) {
-                if (!playing) incActivePuppet().player.play("TEST", true);
-                else incActivePuppet().player.pause("TEST");
-                playing = !playing;
-            }
-
-            if (incToolbarButton("DUMMY", 64)) {
-                import std.random : uniform;
-
-                AnimationLane[] newRandomLaneParam(Parameter param, InterpolateMode mode, int frames, int sep = 5) {
-                    int iter = param.isVec2 + 1;
-                    AnimationLane[] p;
-
-                    foreach(i; 0..iter) {
-                        p ~= AnimationLane(
-                            param.uuid,
-                            new AnimationParameterRef(param, i), 
-                            [], 
-                            mode
-                        );
-
-                        osseed(uniform(0, uint.max));
-                        foreach(x; 1..(frames-1)/sep) {
-                            p[i].frames ~= Keyframe(x*sep, (1+osnoise2(cast(float)x, 0))/2.0, 0);
-                        }
-                    }
-                    return p;
+        igPushID("TopBar");
+            igBeginDisabled(!inAnimMode);
+            if (incBeginInnerToolbar(24)) {
+                
+                if (incToolbarButton("", 32)) {
+                    incActivePuppet().player.stopAll(true);
+                    playing = false;
                 }
 
-                Animation a = Animation(
-                    0.100, false, 1, [], 100, 0, 0
-                );
+                if (incToolbarButton(playing ? "" : "", 32)) {
+                    if (!playing) incActivePuppet().player.play("TEST", true);
+                    else incActivePuppet().player.pause("TEST");
+                    playing = !playing;
+                }
 
-                size_t i = 0;
-                // foreach(ref param; incActivePuppet().parameters) {
-                //     if (auto group = cast(ExParameterGroup)param) {
-                //         foreach(ref child; group.children) {
-                //             a.lanes ~= newRandomLaneParam(child, InterpolateMode.Cubic, 100);
-                //         }
-                //     } else a.lanes ~= newRandomLaneParam(param, InterpolateMode.Cubic, 100);
+                if (incToolbarButton("DUMMY", 64)) {
+                    import std.random : uniform;
+
+                    AnimationLane[] newRandomLaneParam(Parameter param, InterpolateMode mode, int frames, int sep = 5) {
+                        int iter = param.isVec2 + 1;
+                        AnimationLane[] p;
+
+                        foreach(i; 0..iter) {
+                            p ~= AnimationLane(
+                                param.uuid,
+                                new AnimationParameterRef(param, i), 
+                                [], 
+                                mode
+                            );
+
+                            osseed(uniform(0, uint.max));
+                            foreach(x; 1..(frames-1)/sep) {
+                                p[i].frames ~= Keyframe(x*sep, (1+osnoise2(cast(float)x, 0))/2.0, 0);
+                            }
+                        }
+                        return p;
+                    }
+
+                    Animation a = Animation(
+                        0.100, false, 1, [], 100, 0, 0
+                    );
+
+                    size_t i = 0;
+                    // foreach(ref param; incActivePuppet().parameters) {
+                    //     if (auto group = cast(ExParameterGroup)param) {
+                    //         foreach(ref child; group.children) {
+                    //             a.lanes ~= newRandomLaneParam(child, InterpolateMode.Cubic, 100);
+                    //         }
+                    //     } else a.lanes ~= newRandomLaneParam(param, InterpolateMode.Cubic, 100);
+                        
+                    // }
+
+                    ExPuppet expuppet = cast(ExPuppet)incActivePuppet();
                     
-                // }
+                    a.lanes ~= newRandomLaneParam(expuppet.findParameter("Head:: Roll"), InterpolateMode.Stepped, 100);
+                    a.lanes ~= newRandomLaneParam(expuppet.findParameter("Head:: Yaw-Pitch"), InterpolateMode.Stepped, 100);
+                    a.lanes ~= newRandomLaneParam(expuppet.findParameter("Body:: Roll"), InterpolateMode.Stepped, 100);
+                    a.lanes ~= newRandomLaneParam(expuppet.findParameter("Arm:: Left:: Move"), InterpolateMode.Stepped, 100);
+                    a.lanes ~= newRandomLaneParam(expuppet.findParameter("Arm:: Right:: Move"), InterpolateMode.Stepped, 100);
+                    a.lanes ~= newRandomLaneParam(expuppet.findParameter("Body:: X:: Move"), InterpolateMode.Stepped, 100);
 
-                ExPuppet expuppet = cast(ExPuppet)incActivePuppet();
-                
-                a.lanes ~= newRandomLaneParam(expuppet.findParameter("Head:: Roll"), InterpolateMode.Stepped, 100);
-                a.lanes ~= newRandomLaneParam(expuppet.findParameter("Head:: Yaw-Pitch"), InterpolateMode.Stepped, 100);
-                a.lanes ~= newRandomLaneParam(expuppet.findParameter("Body:: Roll"), InterpolateMode.Stepped, 100);
-                a.lanes ~= newRandomLaneParam(expuppet.findParameter("Arm:: Left:: Move"), InterpolateMode.Stepped, 100);
-                a.lanes ~= newRandomLaneParam(expuppet.findParameter("Arm:: Right:: Move"), InterpolateMode.Stepped, 100);
-                a.lanes ~= newRandomLaneParam(expuppet.findParameter("Body:: X:: Move"), InterpolateMode.Stepped, 100);
+                    incActivePuppet().getAnimations()["TEST"] = a;
+                    incAnimationSet(incActivePuppet().getAnimations()["TEST"]);
 
-                incActivePuppet().getAnimations()["TEST"] = a;
-                incAnimationSet(incActivePuppet().getAnimations()["TEST"]);
-
-                incActivePuppet().player.set("TEST", true);
+                    incActivePuppet().player.set("TEST", true);
+                }
             }
+            incEndInnerToolbar();
+            igEndDisabled();
+        igPopID();
 
-            incToolbarSpacer(4);
-
-            import std.format;
-            incText("%s".format(incActivePuppet().player.getAnimTime()));
-        }
-        incEndInnerToolbar();
-        igEndDisabled();
+        // Widget height is used in all cases
+        widgetHeight = incAvailableSpace().y-24;
 
         // Don't render contents if not in animation edit mode
+        if (inAnimMode) {
+            if (igIsWindowHovered(ImGuiHoveredFlags.ChildWindows)) {
+
+                if ((igGetIO().KeyMods & ImGuiModFlags.Ctrl) == ImGuiModFlags.Ctrl) {
+                    
+                    float delta = (igGetIO().MouseWheel*2*zoom)*deltaTime();
+                    zoom = clamp(zoom+delta, TIMELINE_MIN_ZOOM, TIMELINE_MAX_ZOOM);
+                } else {
+
+                    float delta = (igGetIO().MouseWheel*1024)*deltaTime();
+                    scroll -= delta;
+                }
+            }
+
+            drawHeaders();
+            drawTracks();
+        } else {
+            incDummy(ImVec2(0, widgetHeight-3));
+        }
+
+        igPushID("BottomBar");
+            if (incBeginInnerToolbar(24, false, false)) {
+
+                // Align text
+                igSetCursorPosY(6);
+
+                float t = incActivePuppet().player.getAnimTime();
+                int s = cast(int)t;
+                int ms = cast(int)((t-cast(float)s)*100);
+
+                import std.format;
+                incText("%ss %sms".format(s, ms));
+            }
+            incEndInnerToolbar();
+        igPopID();
+
         if (!inAnimMode) {
             incLabelOver(_("Not in Animation Edit mode..."), ImVec2(0, 0), true);
             return;
         }
-
-        if (igIsWindowHovered(ImGuiHoveredFlags.ChildWindows)) {
-
-            if ((igGetIO().KeyMods & ImGuiModFlags.Ctrl) == ImGuiModFlags.Ctrl) {
-                
-                float delta = (igGetIO().MouseWheel*2*zoom)*deltaTime();
-                zoom = clamp(zoom+delta, TIMELINE_MIN_ZOOM, TIMELINE_MAX_ZOOM);
-            } else {
-
-                float delta = (igGetIO().MouseWheel*1024)*deltaTime();
-                scroll -= delta;
-            }
-        }
-
-        widgetHeight = incAvailableSpace().y;
-
-        drawHeaders();
-        drawTracks();
     }
 
 public:
     this() {
         super("Timeline", _("Timeline"), false);
+        this.flags |= ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse;
     }
 }
 
