569b53d7d73ddc1b28252726066049604e0320c3
LunaTheFoxgirl <cli-luna@protonmail.com>
2023-04-29 13:58:44 +0200

    Fix node addition position parenting

--- source/creator/actions/node.d
+++ source/creator/actions/node.d
@@ -228,6 +228,7 @@ void incAddChildWithHistory(Node n, Node to, string name=null) {
     ));
 
     n.insertInto(to, Node.OFFSET_START);
+    n.localTransform.clear();
     if (name is null) n.name = _("Unnamed ")~_(n.typeId());
     else n.name = name;
     incActivePuppet().rescanNodes();
