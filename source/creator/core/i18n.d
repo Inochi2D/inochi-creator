b3fb49eb637e5536dad3de8fb264aa694e1b85c9
LunaTheFoxgirl <luna@foxgirls.gay>
2025-06-16 00:45:30 +0200

    Clean up translation subsystem

--- source/creator/core/i18n.d
+++ source/creator/core/i18n.d
@@ -1,13 +1,13 @@
 module creator.core.i18n;
 import creator.core;
 import i18n.culture;
-import i18n;
-import i18n.tr;
+import i18n.tr : i18nLoadLanguage, i18nClearLanguage, i18nGetLanguageName;
 import std.file;
 import std.path;
 import std.string;
 import std.algorithm : sort;
 import std.uni : icmp;
+import nulib.string;
 
 /+
     HACK: This little comment tricks genpot to generate our LANG_NAME entry.
@@ -18,10 +18,162 @@ import std.uni : icmp;
     _("LANG_NAME")
 +/
 
-private {
-    TLEntry[] localeFiles;
+/**
+    A localized string instance.
+*/
+struct LocalizedString {
+    alias text this;
 
-    string incGetCultureExpression(string langcode) {
+    string key;
+    nstring text;
+}
+
+/**
+    Wraps i18n-d with a LocalizedString
+*/
+LocalizedString _(string toTranslate) {
+    import tr = i18n.tr;
+    return LocalizedString(toTranslate, tr._(toTranslate));
+}
+
+/**
+    Wraps i18n-d with a LocalizedString
+*/
+LocalizedString __(string toTranslate) {
+    import tr = i18n.tr;
+    return LocalizedString(toTranslate, tr._(toTranslate));
+}
+
+/**
+    An entry within the locale store.
+*/
+struct LocaleEntry {
+public:
+    nstring humanName;
+    string code;
+    string file;
+    string path;
+}
+
+/**
+    The locale store.
+*/
+struct LocaleStore {
+private:
+    size_t active;
+    LocaleEntry[] entries;
+
+    void scan(string path) {
+        // Skip non-existent paths
+        if (!path.exists) return;
+
+        foreach(DirEntry entry; dirEntries(path, "*.mo", SpanMode.shallow)) {
+            
+            // Get langcode from filename
+            string langcode = baseName(stripExtension(entry.name));
+
+            // Skip langcodes we don't know
+            if (!i18nValidateCultureCode(langcode)) continue;
+
+            string langName = i18nGetLanguageName(entry.name);
+            if (langName == "<UNKNOWN LANGUAGE>") langName = incGetCultureExpression(langcode);
+            
+            // Add locale
+            entries ~= TLEntry(
+                langName,
+                langName.toStringz,
+                langcode, 
+                entry.name,
+                path
+            );
+        }
+    }
+
+    void markDuplicates() {
+
+        // Skip if only one entry
+        if (entries.length <= 1) return;
+        
+        TLEntry* prevEntry = &entries[0];
+        bool prevIsDup = false;
+
+        foreach(ref entry; entries[1 .. $]) {
+            bool entryIsDup = entry.humanName == prevEntry.humanName;
+
+            // If prevEntry has same humanName as entry before prevEntry, or as this entry,
+            // disambiguate with the source folder
+            if (prevIsDup || entryIsDup) {
+                prevEntry.humanName ~= " (" ~ prevEntry.path ~ ")";
+            }
+
+            prevIsDup = entryIsDup;
+            prevEntry = &entry;
+        }
+
+        if (prevIsDup) {
+            prevEntry.humanName ~= " (" ~ prevEntry.path ~ ")";
+        }
+    }
+    
+    bool compareEntries(LocaleEntry a, LocaleEntry b) {
+        int cmp = icmp(a.humanName, b.humanName);
+        if (cmp == 0) {
+            return a.path < b.path;
+        }
+        return cmp < 0;
+    }
+
+    LocaleEntry* getEntryFor(string code) {
+        foreach(ref entry; entries) {
+            if (entry.code == code)
+                return &entry;
+        }
+
+        return null;
+    }
+
+public:
+
+    /**
+        The current selected locale human name
+    */
+    @property string currentLocaleName() {
+        return this.getCultureExpression(currentLocale);
+    }
+
+    /**
+        The currently selected locale
+    */
+    @property string currentLocale() {
+        string code = AppSettings.get("lang", "en");
+        return code.length == 0 ? "en" : code;
+    }
+    
+    @property void currentLocale(string code) {
+        AppSettings.set("lang", code);
+        
+        // Builtin EN has no .po file
+        if (code.length == 0 || code == "en") {
+            i18nClearLanguage();
+            return;
+        }
+
+        // Other languages do, though.
+        i18nLoadLanguage(this.getEntryFor(code).file);
+    }
+
+    /**
+        List of locales loaded.
+    */
+    @property LocaleEntry[] locales() {
+        return entries[0..$];
+    }
+
+    /**
+        Gets the culture expression for the given language
+        code.
+    */
+    string getCultureExpression(string langcode) {
 
         // Most cases
         foreach(locale; localeFiles) {
@@ -40,145 +192,27 @@ private {
         return i18nGetCultureLanguage(langcode);
     }
 
-    void incLocaleScan(string path) {
+    /**
+        Rescans all of the locales.    
+    */
+    void rescan() {
+        entries.length = 0;
 
-        // Skip non-existent paths
-        if (!path.exists) return;
+        // These exist for testing + user added localization
+        foreach(localePath; AppSettings.localePaths)
+            this.scan(localePath);
+        this.scan(thisExePath().dirName);
 
-        foreach(DirEntry entry; dirEntries(path, "*.mo", SpanMode.shallow)) {
-            
-            // Get langcode from filename
-            string langcode = baseName(stripExtension(entry.name));
-
-            // Skip langcodes we don't know
-            if (!i18nValidateCultureCode(langcode)) continue;
-
-            string langName = i18nGetLanguageName(entry.name);
-            if (langName == "<UNKNOWN LANGUAGE>") langName = incGetCultureExpression(langcode);
-            
-            // Add locale
-            localeFiles ~= TLEntry(
-                langName,
-                langName.toStringz,
-                langcode, 
-                entry.name,
-                path
-            );
-        }
+        // For zip folder exports.
+        this.scan(buildPath(thisExePath().dirName, "i18n"));
+        
+        // On macOS we store the locale in the app bundle under the Resources subdirectory.
+        version(OSX) this.scan(buildPath(thisExePath().dirName, "../Resources/i18n"));
+        
+        // sort the files by human readable name
+        entries.sort!(compareEntries);
+        
+        //disambiguate locales with the same human name
+        this.markDuplicates();
     }
-}
-
-/**
-    Entry in the translations table
-*/
-struct TLEntry {
-public:
-    string humanName;
-    const(char)* humanNameC;
-    string code;
-    string file;
-    string path;
-}
-
-/**
-    Initialize translations
-*/
-void incLocaleInit() {
-
-    // These exist for testing + user added localization
-    foreach(localePath; AppSettings.localePaths)
-        incLocaleScan(localePath);
-    incLocaleScan(thisExePath().dirName);
-
-    // For zip folder exports.
-    incLocaleScan(buildPath(thisExePath().dirName, "i18n"));
-    
-    // On macOS we store the locale in the app bundle under the Resources subdirectory.
-    version(OSX) incLocaleScan(buildPath(thisExePath().dirName, "../Resources/i18n"));
-    
-    // sort the files by human readable name
-    localeFiles.sort!(compareEntries);
-    //disambiguate locales with the same human name
-    markDups(localeFiles);
-}
-
-bool compareEntries(TLEntry a, TLEntry b) {
-    int cmp = icmp(a.humanName, b.humanName);
-    if (cmp == 0) {
-        return a.path < b.path;
-    }
-    return cmp < 0;
-}
-
-/**
-    Disambiguate by source folder for TLEntrys with identical humanNames.
-    Expects an array sorted by humanName
-*/
-void markDups(TLEntry[] entries) {
-
-    // Skip if only one entry
-    if (entries.length <= 1) return;
-    
-    TLEntry* prevEntry = &entries[0];
-    bool prevIsDup = false;
-
-    foreach(ref entry; entries[1 .. $]) {
-        bool entryIsDup = entry.humanName == prevEntry.humanName;
-
-        // If prevEntry has same humanName as entry before prevEntry, or as this entry,
-        // disambiguate with the source folder
-        if (prevIsDup || entryIsDup) {
-            prevEntry.humanName ~= " (" ~ prevEntry.path ~ ")";
-            prevEntry.humanNameC = prevEntry.humanName.toStringz;
-        }
-        prevIsDup = entryIsDup;
-        prevEntry = &entry;
-    }
-
-    if (prevIsDup) {
-        prevEntry.humanName ~= " (" ~ prevEntry.path ~ ")";
-        prevEntry.humanNameC = prevEntry.humanName.toStringz;
-    }
-}
-
-/**
-    Gets the current selected locale human name
-*/
-string incLocaleCurrentName() {
-    string code = AppSettings.get("lang", "en");
-    string currCode = code.length == 0 ? "en": code;
-    return incGetCultureExpression(currCode);
-}
-
-/**
-    Sets the locale for the application
-*/
-void incLocaleSet(string code) {
-    AppSettings.set("lang", code);
-    
-    // Builtin EN has no .po file
-    if (code.length == 0 || code == "en") {
-        i18nClearLanguage();
-        return;
-    }
-
-    // Other languages do, though.
-    i18nLoadLanguage(incLocaleGetEntryFor(code).file);
-}
-
-/**
-    Get locale entry for a code
-*/
-TLEntry* incLocaleGetEntryFor(string code) {
-    foreach(ref entry; localeFiles) {
-        if (entry.code == code) return &entry;
-    }
-    return null;
-}
-
-/**
-    Returns the locale list
-*/
-TLEntry[] incLocaleGetEntries() {
-    return localeFiles;
 }
